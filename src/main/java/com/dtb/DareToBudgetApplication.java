47667080e658131318a78b85e2d4da1852183aac
donzdanowicz <madpz89@gmail.com>
2022-03-13 07:56:49 +0100

    First Commit

---
+++ src/main/java/com/dtb/DareToBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.dtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DareToBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DareToBudgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dtb/DareToBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DareToBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
